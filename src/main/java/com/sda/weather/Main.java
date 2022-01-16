1f805949214248167f42f2c909377fada5f221f0
Michal Paukszto <michal.paukszto@gmail.com>
2022-01-16 21:19:23 +0100

    small remarks

--- src/main/java/com/sda/weather/Main.java
+++ src/main/java/com/sda/weather/Main.java
@@ -5,9 +5,7 @@ import com.sda.weather.frontend.UserInterface;
 public class Main {
 
     public static void main(String[] args) {
-
         UserInterface userInterface = new UserInterface();
         userInterface.run();
     }
-
 }
--- src/main/java/com/sda/weather/frontend/UserInterface.java
+++ src/main/java/com/sda/weather/frontend/UserInterface.java
@@ -11,22 +11,22 @@ public class UserInterface {
 
         while (true) {
             System.out.println("Witaj w aplikacji pogodowej, co chcesz zrobić?");
-            System.out.println("1. Dodać lokalizację do bazy danych");
+            System.out.println("1. Dodać lokalizację do bazy danych"); // todo po prostu lokalizacje (nie musisz zdradzać że do bazy)
             System.out.println("2. Wyświetlić wszystkie dostępne lokalizacje");
-            System.out.println("3. Pobrać pogodę w wybranej lokalizacji");
+            System.out.println("3. Pobrać pogodę w wybranej lokalizacji"); // todo prognozę pogody
             System.out.println("0. Zamknąć aplikacje");
 
             int option = scanner.nextInt();
 
             switch (option) {
                 case 1:
-                    createEntry();
+                    createEntry(); // todo lokalizację, nie entry
                     break;
                 case 2:
                     getAllLocalizations();
                     break;
                 case 3:
-                    getEntry();
+                    getEntry(); // todo getWeatherForecast
                     break;
                 case 0:
                     return;
@@ -36,19 +36,19 @@ public class UserInterface {
     }
 
     private void getAllLocalizations() {
-        // GET: /entry do serwera
+        // GET: /entry do serwera // todo GET: /localization
         // wyświetla lokalizacje
         //todo
     }
 
     private void createEntry() {
-        // POST: /entry do serwera
+        // POST: /entry do serwera  // todo POST: /localization
         // dodanie lokalizacji
         //todo
     }
 
     private void getEntry() {
-        // GET: /entry do serwera
+        // GET: /entry do serwera   // todo GET: /localization/{id}/forecast
         // pobranie rekordu
         //todo
     }
